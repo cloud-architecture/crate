afdfba7f30cfc5158ff3bd44cb4659805b3c9174
Marios Trivyzas <marios@crate.io>
2017-09-26 15:35:25 +0200

    Added Cluster Check for tables that need recreation.
    This check is added to "catch" tables that are created before ES 5 (CrateDB < 2.0.0)
    and suggest to the user to recreate them so that they will be compatible with ES 6.
    The reason is that in ES 6 the backwards compatibility layer is removed, and in Lucene 7
    the bwc layer is moved to solr packages, so it quite some effort will be required to re-enable it on our own.
    Moreover by removing the bwc layer we will be able to use new more performant Lucene queries in some cases (eg. geo-queries).
    
    The TablesNeedUpgradeSysCheck will inform the user to run OPTIMIZE TABLE `table_ident` WITH (upgrade_segments=true)
    for all tables that are not in the current Lucene major version. Since Currently (ES 5.x) Lucene version is 6.x the
    check will "catch" tables that are created before ES 5.x (Lucene 5.x) but those tables are also "cought" by the TablesNeedRecreationSysCheck.
    Therefore there is no way we can have tables to trigger the TablesNeedUpgradeSysCheck and the relevant integ tests are marked
    with @Ignore and a TODO is added as a reminder to renable them after the upgrade to ES 6.

--- sql/src/test/java/io/crate/operation/reference/sys/check/cluster/LuceneVersionChecksTest.java
+++ sql/src/test/java/io/crate/operation/reference/sys/check/cluster/LuceneVersionChecksTest.java
@@ -23,11 +23,32 @@
 package io.crate.operation.reference.sys.check.cluster;
 
 import io.crate.test.integration.CrateUnitTest;
+import org.elasticsearch.Version;
+import org.elasticsearch.cluster.metadata.IndexMetaData;
+import org.elasticsearch.common.settings.Settings;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
 
 public class LuceneVersionChecksTest extends CrateUnitTest {
+
+    @Test
+    public void testRecreationRequired() {
+        assertThat(LuceneVersionChecks.isRecreationRequired(null), is(false));
+        IndexMetaData recreationRequired = new IndexMetaData.Builder("testRecreationRequired")
+            .settings(Settings.builder().put(IndexMetaData.SETTING_VERSION_CREATED, Version.V_2_4_2).build())
+            .numberOfShards(5)
+            .numberOfReplicas(2)
+            .build();
+        assertThat(LuceneVersionChecks.isRecreationRequired(recreationRequired), is(true));
+
+        IndexMetaData noRecreationRequired = new IndexMetaData.Builder("testNoRecreationRequired")
+            .settings(Settings.builder().put(IndexMetaData.SETTING_VERSION_CREATED, Version.V_5_0_1).build())
+            .numberOfShards(5)
+            .numberOfReplicas(2)
+            .build();
+        assertThat(LuceneVersionChecks.isRecreationRequired(noRecreationRequired), is(false));
+    }
 
     @Test
     public void testUpgradeRequired() {
